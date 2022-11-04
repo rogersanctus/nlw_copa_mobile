0baaa1949fd3b1569efd5ac45cda74c02dea4505
Rogério Santos <rogersanctus@gmail.com>
2022-11-03 23:14:15 -0300

    feat: adds the content of the SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,24 @@
-import { Text } from "native-base";
+import Logo from "../assets/logo.svg"
+import { Button } from "../components/Button";
+import { Fontisto } from '@expo/vector-icons'
+import { Text, Icon } from "native-base";
 
 export function SignIn() {
     return (
-        <Text color="white" fontSize={24} fontFamily="heading">
-            Sign In
-        </Text>
+        <>
+            <Logo width={212} height={40} />
+            <Button
+                text="Entrar com Google"
+                variant="secondary"
+                leftIcon={
+                    <Icon as={Fontisto} name="google" color="white" size="md" />
+                }
+                mt={12}
+            />
+            <Text color="gray.200" textAlign="center" mt={4}>
+                Não utilizamos nenhuma informação além {'\n'}
+                do seu e-mail para a criação de sua conta.
+            </Text>
+        </>
     )
 }
