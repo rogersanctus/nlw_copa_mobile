a09f6cae6eb5d5dcc6435d9dbf43ed0ddd0cf2fa
Rogério Santos <rogersanctus@gmail.com>
2022-11-07 17:18:02 -0300

    feat: navigate to FindPoll when pressed the find button

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -3,8 +3,12 @@ import { Header } from "../components/Header"
 import { Button } from "../components/Button"
 import { Icon } from "native-base";
 import { Octicons } from '@expo/vector-icons'
+import { useNavigation } from "@react-navigation/native";
 
 export function Pools() {
+
+    const navigation = useNavigation()
+
     return (
         <VStack flex={1} bg="gray.900">
             <Header title="Meus bolões" showBackButton />
@@ -15,6 +19,7 @@ export function Pools() {
                     leftIcon={
                         <Icon as={Octicons} name="search" color="black" size="md" />
                     }
+                    onPress={() => navigation.navigate('find')}
                 />
                 <Divider my={4} bg="gray.600" />
                 <Text color="gray.200">Just a text</Text>
