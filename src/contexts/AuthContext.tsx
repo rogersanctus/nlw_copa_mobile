369101036743b86e96d071656eedf5ca9152f679
Rogério Santos <rogersanctus@gmail.com>
2022-11-07 17:01:59 -0300

    feat: implementation of content of 'Aula 4' chore: adds dependencies to deal with routes navigation

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,7 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { makeRedirectUri } from "expo-auth-session";
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser'
 
 interface AuthProviderProps {
     children: ReactNode
@@ -11,6 +14,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
     user: UserProps
+    isUserLoading: boolean
     signIn: () => Promise<void>
 }
 
@@ -18,18 +22,47 @@ export const AuthContext = createContext({
 
 } as AuthContextDataProps)
 
-export function AuthContextProvider({ children }: AuthProviderProps) {
-    async function signIn() {
+WebBrowser.maybeCompleteAuthSession()
 
+export function AuthContextProvider({ children }: AuthProviderProps) {
+    const [ isUserLoading, setIsUserLoading ] = useState(false)
+    const [ user, setUser ] = useState({} as UserProps)
+    const redirectUri = makeRedirectUri({ useProxy: true })
+
+    const [ request, response, promptAsync ] = Google.useAuthRequest({
+        clientId: '323853255012-ifnpe5r2l4k0ljphakoiul3re61gttr6.apps.googleusercontent.com',
+        clientSecret: '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',
+        redirectUri,
+        scopes: ['profile', 'email']
+    })
+
+    function signInWithGoogle(token: string) {
+        console.log('TOKEN: ', token)
+    }
+
+    useEffect(() => {
+        if(response?.type === 'success' && response.authentication?.accessToken) {
+            signInWithGoogle(response.authentication.accessToken)
+        }
+    }, [response])
+
+    async function signIn() {
+        try {
+            setIsUserLoading(true)
+            await promptAsync()
+        } catch(error) {
+            console.error(error)
+            throw error
+        } finally {
+            setIsUserLoading(false)
+        }
     }
 
     return (
         <AuthContext.Provider value={{
             signIn: signIn,
-            user: {
-                name: 'Zé da Ema',
-                avatarUrl: 'https://avatars.githubusercontent.com/u/1813125?s=96&v=4'
-            }
+            isUserLoading,
+            user
         }}>
             { children }
         </AuthContext.Provider>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,8 +2,12 @@ import Logo from "../assets/logo.svg"
 import { Button } from "../components/Button";
 import { Fontisto } from '@expo/vector-icons'
 import { Text, Icon } from "native-base";
+import { useAuth } from "../hooks/useAuth";
 
 export function SignIn() {
+
+    const { signIn, user } = useAuth()
+
     return (
         <>
             <Logo width={212} height={40} />
@@ -14,6 +18,7 @@ export function SignIn() {
                     <Icon as={Fontisto} name="google" color="white" size="md" />
                 }
                 mt={12}
+                onPress={signIn}
             />
             <Text color="gray.200" textAlign="center" mt={4}>
                 Não utilizamos nenhuma informação além {'\n'}
