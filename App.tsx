f036fe3ffa0d90bb59927ea19e175d70e95eaed3
Rogério Santos <rogersanctus@gmail.com>
2022-11-07 17:07:25 -0300

    feat: finally adds the route navigation to the app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { Center, NativeBaseProvider, StatusBar } from 'native-base'
+import { NativeBaseProvider, StatusBar } from 'native-base'
 
 import { THEME } from './src/styles/theme'
 import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 import { Loading } from './src/components/Loading'
 import React from 'react'
-import { SignIn } from './src/screens/SignIn'
 import { AuthContextProvider } from './src/contexts/AuthContext'
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -14,11 +14,9 @@ export default function App() {
     <NativeBaseProvider theme={THEME}>
       <AuthContextProvider>
         <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
-        <Center flex={1} bgColor="gray.900" p={7}>
-          {
-            fontsLoaded ? <SignIn /> : <Loading />
-          }
-        </Center>
+        {
+          fontsLoaded ? <Routes /> : <Loading />
+        }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
