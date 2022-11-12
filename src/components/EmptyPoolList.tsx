ab1e187d6d55b91cf86a946dcf7f33f15147d2d6
Rogério Santos <rogersanctus@gmail.com>
2022-11-11 23:53:16 -0300

    feat: implements the navigation for each Pressable

--- src/components/EmptyPoolList.tsx
+++ src/components/EmptyPoolList.tsx
@@ -1,26 +1,33 @@
+import {useNavigation} from '@react-navigation/native';
 import {Row, Text, Pressable} from 'native-base';
 
-export function EmptyPoolList() {
+export function EmptyPoolList({showFindPool = false}: {showFindPool: boolean}) {
+  const {navigate} = useNavigation();
+
   return (
     <Row flexWrap="wrap" justifyContent="center">
       <Text color="white" fontSize="sm" textAlign="center">
-        Você ainda não está participando de {'\n'} nenhum bolão, que tal
+        Você ainda não está participando de nenhum bolão, que tal
       </Text>
 
-      <Pressable>
-        <Text
-          textDecorationLine="underline"
-          color="yellow.500"
-          textDecoration="underline">
-          buscar um por código
-        </Text>
-      </Pressable>
+      {showFindPool ? (
+        <>
+          <Pressable onPress={() => navigate('find')}>
+            <Text
+              textDecorationLine="underline"
+              color="yellow.500"
+              textDecoration="underline">
+              buscar um por código
+            </Text>
+          </Pressable>
 
-      <Text color="white" fontSize="sm" textAlign="center" mx={1}>
-        ou
-      </Text>
+          <Text color="white" fontSize="sm" textAlign="center" mx={1}>
+            ou
+          </Text>
+        </>
+      ) : null}
 
-      <Pressable>
+      <Pressable onPress={() => navigate('new')}>
         <Text textDecorationLine="underline" color="yellow.500">
           criar um novo
         </Text>
