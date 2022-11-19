10ea849b060420c5d90b134619415177e4101b73
Rogério Santos <rogersanctus@gmail.com>
2022-11-19 19:13:04 -0300

    feat: adds messages translations and group it with others in the translation group

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,6 +1,7 @@
 import 'intl-pluralrules';
 import i18next from 'i18next';
 import countriesPtBr from '../../locales/pt-BR/countries.json';
+import messagesBr from '../../locales/pt-BR/messages.json';
 
 export const i18n = {t: (v?: string | null) => v ?? ''};
 i18next
@@ -8,7 +9,12 @@ i18next
     //compatibilityJSON: 'v3',
     lng: 'pt-BR',
     resources: {
-      'pt-BR': {translation: countriesPtBr},
+      'pt-BR': {
+        translation: {
+          countries: countriesPtBr,
+          messages: messagesBr,
+        },
+      },
     },
   })
   .then(t => (i18n.t = t));
